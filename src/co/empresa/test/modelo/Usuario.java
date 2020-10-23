1a224efd5de04ffdb3eee1ace395802312b3d073
jarbirS <61531454+jarbirS@users.noreply.github.com>
2020-10-23 02:32:58 -0500

    Add files via upload

---
+++ src/co/empresa/test/modelo/Usuario.java
@@ -0,0 +1,67 @@
+package co.empresa.test.modelo;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario implements Serializable {
+
+	private Integer id;
+	private String nombre;
+	private String email;
+	private String pais;
+	
+	
+	public Usuario() {
+		
+	}
+	
+	public Usuario ( String nombre , String email, String pais) {
+		
+		this.nombre=nombre;
+		this.email=email;
+		this.pais=pais;
+	}
+	
+	public Usuario (Integer id, String nombre , String email, String pais) {
+		this.id=id;
+		this.nombre=nombre;
+		this.email=email;
+		this.pais=pais;
+	}
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPais() {
+		return pais;
+	}
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+	
+	
+}
